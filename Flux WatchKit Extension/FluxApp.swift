7c978905d0e1696830b3612554804068260aefc1
Christian Skorobogatow <christiansko@icloud.com>
2022-04-10 15:18:59 +0200

    Initial Commit

---
+++ Flux WatchKit Extension/FluxApp.swift
@@ -0,0 +1,21 @@
+//
+//  FluxApp.swift
+//  Flux WatchKit Extension
+//
+//  Created by Christian Skorobogatow on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct FluxApp: App {
+    @SceneBuilder var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+
+        WKNotificationScene(controller: NotificationController.self, category: "myCategory")
+    }
+}
